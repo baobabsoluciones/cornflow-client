7177f2da808c469ea5d99ca7dfac15e63ca8737e
pchtsp <pchtsp@gmail.com>
2021-04-26 19:43:04 +0200

    take out schema_dict from properties.

--- cornflow_client/schema/manager.py
+++ cornflow_client/schema/manager.py
@@ -13,25 +13,17 @@ from cornflow_client.constants import JSON_TYPES, DATASCHEMA
 
 class SchemaManager:
     
-    def __init__(self, schema=None):
+    def __init__(self, schema, validator=Draft7Validator):
         """
         Class to help create and manage data schema.
         Once a schema is loaded, allow the validation of data.
         
         :param schema: a json schema
         """
-        self.default_validator = Draft7Validator
-        self.jsonschema = schema if schema is not None else {}
+        self.validator = validator
+        self.jsonschema = schema
         self.types = JSON_TYPES
-        self.schema_dict = self.get_empty_schema()
 
-    @staticmethod
-    def get_empty_schema():
-        """
-        Create un empty schema dict
-        """
-        return {DATASCHEMA: []}
-    
     @classmethod
     def from_filepath(cls, path):
         """
@@ -60,13 +52,12 @@ class SchemaManager:
             raise Warning("No jsonschema has been loaded in the SchemaManager class")
         return loaded
     
-    def get_validation_errors(self, data, validator=None):
+    def get_validation_errors(self, data):
         """
         Validate json data according to the loaded jsonschema and return a list of errors.
         Return an empty list if data is valid.
 
         :param dict data: data to validate.
-        :param validator: A jsonschema IValidator class. If None, will use self.default_validator.
 
         :return: A list of validation errors.
 
@@ -74,27 +65,24 @@ class SchemaManager:
         https://python-jsonschema.readthedocs.io/en/latest/errors/#jsonschema.exceptions.ValidationError
         """
         self.is_schema_loaded()
-    
-        if validator is None:
-            validator = self.default_validator
-        v = validator(self.get_jsonschema())
+
+        v = self.validator(self.get_jsonschema())
 
         if not v.is_valid(data):
             error_list = [e for e in v.iter_errors(data)]
             return error_list
         return []
     
-    def validate_data(self, data, validator=None, print_errors=False):
+    def validate_data(self, data, print_errors=False):
         """
         Validate json data according to the loaded jsonschema.
 
         :param dict data: the data to validate.
-        :param validator: A jsonschema IValidator class. If None, will use self.default_validator.
         :param bool print_errors: If true, will print the errors.
 
         :return: True if data format is valid, else False.
         """
-        errors_list = self.get_validation_errors(data, validator=validator)
+        errors_list = self.get_validation_errors(data)
         
         if print_errors:
             for e in errors_list:
@@ -102,60 +90,61 @@ class SchemaManager:
         
         return len(errors_list) == 0
     
-    def get_file_errors(self, path, validator=None):
+    def get_file_errors(self, path):
         """
         Get json file errors according to the loaded jsonschema.
 
         :param path the file path
-        :param validator A jsonschema IValidator class. If None, will use self.default_validator.
 
         :return: A list of validation errors.
         For more details about the error format, see:
         https://python-jsonschema.readthedocs.io/en/latest/errors/#jsonschema.exceptions.ValidationError
         """
         data = self.load_json(path)
-        return self.get_validation_errors(data, validator=validator)
+        return self.get_validation_errors(data)
     
-    def validate_file(self, path, validator=None, print_errors=False):
+    def validate_file(self, path, print_errors=False):
         """
         Validate a json file according to the loaded jsonschema.
         
         :param path the file path
-        :param validator A jsonschema IValidator class. If None, will use self.default_validator.
         :param print_errors: If true, will print the errors.
         
         :return: True if the data is valid and False if it is not.
         """
         data = self.load_json(path)
-        return self.validate_data(data, validator=validator, print_errors=print_errors)
-    
-    def jsonschema_to_dict(self):
+        return self.validate_data(data, print_errors=print_errors)
+
+    def to_dict_schema(self):
         """
-        Transform a jsonschema into a dictionary and store it in self.schema_dict
+        Transform a jsonschema into a dictionary format
         
         :return: The schema dictionary
         """ 
         jsonschema = self.get_jsonschema()
-        
-        if not self.is_schema_loaded():
-            return self.schema_dict
 
-        self.schema_dict = self.get_empty_schema()
+        schema_dict = self.get_empty_schema()
 
         if "definitions" in jsonschema:
             for item in jsonschema["definitions"].items():
-                self._get_element_dict(item)
+                self._get_element_dict(schema_dict=schema_dict, item=item)
 
         if "properties" in jsonschema:
             for item in jsonschema["properties"].items():
-                self._get_element_dict(item)
-                self._create_data_schema(item, required_list=jsonschema.get('required'))
+                self._get_element_dict(schema_dict=schema_dict, item=item)
+                self._create_data_schema(schema_dict=schema_dict, item=item, required_list=jsonschema.get('required'))
 
-        return self.schema_dict
+        return schema_dict
 
-    def dict_to_flask(self):
+    jsonschema_to_dict = to_dict_schema
+
+    @property
+    def schema_dict(self):
+        return self.to_dict_schema()
+
+    def to_marshmallow(self):
         """
-        Create flask marshmallow schemas from self.dict_schema
+        Create marshmallow schemas
         
         :return: a dict containing the flask marshmallow schemas
         :rtype: Schema()
@@ -171,27 +160,18 @@ class SchemaManager:
             result_dict[key] = gen_schema(key, params1, result_dict)
         
         return result_dict[DATASCHEMA]
-    
+
+    dict_to_flask = to_marshmallow
+
     def jsonschema_to_flask(self):
         """
         Create marshmallow schemas from the jsonschema
 
         :return: a dict containing the marshmallow schema
         """
-        self.jsonschema_to_dict()
         return self.dict_to_flask()
-        
-    def load_schema(self, path):
-        """
-        Load a json schema into self.jsonschema.
 
-        :param path: the path to the json file containing the schema.json
-
-        return the jsonschema
-        """
-        self.jsonschema = self.load_json(path)
-        self.schema_dict = self.get_empty_schema()
-        return self.jsonschema
+    load_schema = from_filepath
 
     def export_schema_dict(self, path):
         """
@@ -227,9 +207,9 @@ class SchemaManager:
     """
     Functions used to translate jsonschema to schema_dict
     """
-    def _create_data_schema(self, item, required_list=None):
+    def _create_data_schema(self, schema_dict, item, required_list=None):
         """
-        Add a schema to self.schema_dict["DataSchema"]
+        Add a schema to schema_dict["DataSchema"]
 
         :param item: (key, value) of a dict. The key contains the name of the schema
             and the value contains its content.
@@ -244,11 +224,11 @@ class SchemaManager:
                        type=self._get_type_or_new_schema(item),
                        many=("type" in content and content["type"] == "array"),
                        required=name in required_list)]
-        self.schema_dict[DATASCHEMA] += schema
+        schema_dict[DATASCHEMA] += schema
     
         return schema
 
-    def _get_element_dict(self, item, required_list=None):
+    def _get_element_dict(self, schema_dict, item, required_list=None):
         """
         Parse an item (key, value) from the jsonschema and return the corresponding dict.
         
@@ -276,21 +256,21 @@ class SchemaManager:
         if content["type"] == "object":
             return {
                 "name": name,
-                "type": self._get_object_schema(item),
+                "type": self._get_object_schema(schema_dict=schema_dict, item=item),
                 "many": False,
                 "required": (name in required_list)
             }
         elif content["type"] == "array":
             return {
                 "name": name,
-                "type": self._get_array_schema(item),
+                "type": self._get_array_schema(schema_dict=schema_dict, item=item),
                 "many": True,
                 "required": (name in required_list),
             }
         else:
             return self._get_field_dict(item, required_list)
     
-    def _get_object_schema(self, item):
+    def _get_object_schema(self, schema_dict, item):
         """
         Transform an object item from the jsonschema in a dict for the schema_dict and update self.schema_dict.
         In jsonschema objects are similar to python dict.
@@ -306,14 +286,14 @@ class SchemaManager:
         :return: The schema name
         """
         name, content = item
-        schema_name = self._get_new_schema_name(name)
+        schema_name = self._get_new_schema_name(schema_dict=schema_dict, name=name)
         ell = \
             {schema_name: [self._get_element_dict(i, self._get_required(content))
                            for i in content["properties"].items()]}
-        self.schema_dict.update(ell)
+        schema_dict.update(ell)
         return schema_name
     
-    def _get_array_schema(self, item):
+    def _get_array_schema(self, schema_dict, item):
         """
         Transform a array item from the jsonschema in a dict for the schema_dict and update self.schema_dict.
         In jsonschema arrays are similar to python lists.
@@ -330,9 +310,9 @@ class SchemaManager:
         """
         name, content = item
         content = content['items']
-        schema_name = self._get_new_schema_name(name)
+        schema_name = self._get_new_schema_name(schema_dict=schema_dict, name=name)
         if "type" in content and content["type"] == "object":
-            self.schema_dict.update(
+            schema_dict.update(
                 {schema_name: [self._get_element_dict(i, self._get_required(content))
                                for i in content["properties"].items()]}
             )
@@ -341,7 +321,7 @@ class SchemaManager:
         elif "type" in content and content["type"] != "array":
             return self._get_type(content["type"])
         else:
-            self.schema_dict.update(
+            schema_dict.update(
                 {schema_name: [self._get_element_dict(i, self._get_required(content)) for i in content.items()]})
         return schema_name
        
@@ -367,7 +347,7 @@ class SchemaManager:
     def _get_ref(self, item):
         """
         Get the name of the schema for a jsonschema reference.
-        jsonschema definitions are parsed first and corresponign schema are created so a schema should exist
+        jsonschema definitions are parsed first and corresponding schema are created so a schema should exist
          corresponding to the reference.
         
         :param item: The jsonschema item in format (key, value)
@@ -382,8 +362,6 @@ class SchemaManager:
     def _get_type_or_new_schema(self, item):
         """
         returns a new schema or a type depending on the json_type
-
-        :param json_type: the type in jsonschema
         """
         name, content = item
         if 'type' not in content or content["type"] == 'object':
@@ -412,7 +390,7 @@ class SchemaManager:
     @staticmethod
     def _get_schema_name(name, n=0):
         """
-        Transform an element name into a schema name in order to create a schema corresponing to an object or array.
+        Transform an element name into a schema name in order to create a schema corresponding to an object or array.
         The schema name use the following format:
         [name][n]Schema (for example if name is "values" and n is 3: Values3Schema)
         
@@ -426,11 +404,11 @@ class SchemaManager:
         else:
             return name.capitalize() + str(n) + "Schema"
     
-    def _get_new_schema_name(self, name, n=0):
+    def _get_new_schema_name(self, schema_dict, name, n=0):
         try_name = self._get_schema_name(name, n)
         
-        if try_name in self.schema_dict:
-            return self._get_new_schema_name(name, n+1)
+        if try_name in schema_dict:
+            return self._get_new_schema_name(schema_dict=schema_dict, name=name, n=n+1)
         else:
             return try_name
     
@@ -466,3 +444,10 @@ class SchemaManager:
     def save_json(data, path):
         with open(path, 'w') as outfile:
             json.dump(data, outfile)
+
+    @staticmethod
+    def get_empty_schema():
+        """
+        Create un empty schema dict
+        """
+        return {DATASCHEMA: []}
