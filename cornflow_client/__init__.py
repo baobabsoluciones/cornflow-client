2484076bf74993aad871aec96a295d43e30a1625
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2021-05-31 09:35:22 +0200

    Modify get_pulp_jsonschema function to get test data from cornflow-client too

--- cornflow_client/__init__.py
+++ cornflow_client/__init__.py
@@ -4,8 +4,11 @@ import json
 import os
 
 
-def get_pulp_jsonschema(filename="pulp_json_schema.json"):
-    filename = os.path.join(os.path.dirname(__file__), "data", filename)
+def get_pulp_jsonschema(filename="pulp_json_schema.json", folder='main'):
+    if folder == "main":
+        filename = os.path.join(os.path.dirname(__file__), "data", filename)
+    elif folder == "test":
+        filename = os.path.join(os.path.dirname(__file__), "tests", "data", filename)
     with open(filename, "r") as f:
         content = json.load(f)
     return content
