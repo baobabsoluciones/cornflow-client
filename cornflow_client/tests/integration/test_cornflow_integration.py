85572f2e02935e3dec05aab068817413c4b3cea3
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-02-21 21:37:29 +0100

    Added some integration tests

--- cornflow_client/tests/integration/test_cornflow_integration.py
+++ cornflow_client/tests/integration/test_cornflow_integration.py
@@ -5,6 +5,9 @@ from cornflow_client import CornFlow
 class TestCornflowClient(TestCase):
     def setUp(self):
         self.client = CornFlow(url="http://127.0.0.1:5050/")
+        login_result = self.client.login("user", "UserPassword1!")
+        self.assertIn("user_id", login_result.keys())
+        self.assertIn("token", login_result.keys())
 
     def tearDown(self):
         pass
@@ -13,3 +16,11 @@ class TestCornflowClient(TestCase):
         response = self.client.is_alive()
         self.assertEqual(response["cornflow_status"], "healthy")
         self.assertEqual(response["airflow_status"], "healthy")
+
+    def test_sign_up(self):
+        response = self.client.sign_up(
+            "test_username", "test_username@cornflow.org", "TestPassword2!"
+        )
+        self.assertIn("user_id", response.json().keys())
+        self.assertIn("token", response.json().keys())
+        self.assertEqual(201, response.status_code)
