64841481cdec13b9e194818993560336eef5ca08
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-02-22 12:34:49 +0100

    Fixed tests errors

--- cornflow_client/tests/integration/test_cornflow_integration.py
+++ cornflow_client/tests/integration/test_cornflow_integration.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 
 from cornflow_client import CornFlow
 from cornflow_client.constants import STATUS_OPTIMAL, STATUS_NOT_SOLVED
+from cornflow_client.schema.tools import get_pulp_jsonschema
 from cornflow_client.tests.const import PUBLIC_DAGS, PULP_EXAMPLE
 
 path_to_tests_dir = os.path.dirname(os.path.abspath(__file__))
@@ -388,21 +389,24 @@ class TestCornflowClientUser(TestCase):
 
     def test_delete_one_instance(self):
         instance = self.test_create_instance()
-        response = self.client.delete_one_instance(instance["instance"])
+        response = self.client.delete_one_instance(instance["id"])
         self.assertEqual(200, response.status_code)
         self.assertEqual("The object has been deleted", response.json()["message"])
 
     def test_get_schema(self):
         response = self.client.get_schema("solve_model_dag")
-        schema = _load_file(_get_file("../../data/pulp_json_schema.json"))
+        schema = {
+            "config": get_pulp_jsonschema("solver_config.json"),
+            "instance": get_pulp_jsonschema(),
+            "solution": get_pulp_jsonschema(),
+        }
         self.assertDictEqual(schema, response)
 
     def test_get_all_schemas(self):
         response = self.client.get_all_schemas()
-        read_schemas = [v for v in response.values()]
         schemas = PUBLIC_DAGS
         for schema in schemas:
-            self.assertIn(schema, read_schemas)
+            self.assertIn(schema, response)
 
 
 class TestCornflowClientAdmin(TestCase):
