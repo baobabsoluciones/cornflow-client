b86416bb985861cc491ceef96521ba67faf49592
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2022-02-23 17:04:49 +0100

    More integration tests for the airflow api

--- cornflow_client/tests/integration/test_airflow_integration.py
+++ cornflow_client/tests/integration/test_airflow_integration.py
@@ -1,6 +1,28 @@
+"""
+
+"""
+import json
+import os
+import time
 from unittest import TestCase
 
+from cornflow_client import CornFlow
 from cornflow_client.airflow.api import Airflow
+from cornflow_client.constants import STATUS_OPTIMAL, STATUS_NOT_SOLVED
+from cornflow_client.tests.const import PULP_EXAMPLE
+
+# Constants
+path_to_tests_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Helper functions
+def _load_file(_file):
+    with open(_file) as f:
+        temp = json.load(f)
+    return temp
+
+
+def _get_file(relative_path):
+    return os.path.join(path_to_tests_dir, relative_path)
 
 
 class TestAirflowClient(TestCase):
@@ -23,3 +45,33 @@ class TestAirflowClient(TestCase):
     def test_bad_connection(self):
         client = Airflow(url="http://127.0.0.1:8088", user="admin", pwd="admin!")
         self.assertFalse(client.is_alive())
+
+    def test_update_schemas(self):
+        response = self.client.update_schemas()
+        self.assertEqual(200, response.status_code)
+
+    def test_update_dag_registry(self):
+        response = self.client.update_dag_registry()
+        self.assertEqual(200, response.status_code)
+
+    def test_run_dag(self):
+        data = _load_file(PULP_EXAMPLE)
+        cf_client = CornFlow(url="http://127.0.0.1:5050/")
+        cf_login = cf_client.login("user", "UserPassword1!")
+        instance = cf_client.create_instance(data, "test_example", "test_description")
+        execution = cf_client.create_execution(
+            instance_id=instance["id"],
+            config={"solver": "PULP_CBC_CMD", "timeLimit": 60},
+            name="test_execution",
+            description="execution_description",
+            schema="solve_model_dag",
+            run=False,
+        )
+        response = self.client.run_dag(execution_id=execution["id"])
+        self.assertEqual(200, response.status_code)
+        self.assertIn("dag_run_id", response.json().keys())
+        status = cf_client.get_status(execution_id=execution["id"])
+        self.assertEqual(STATUS_NOT_SOLVED, response["state"])
+        time.sleep(10)
+        status = cf_client.get_status(execution_id=execution["id"])
+        self.assertEqual(STATUS_OPTIMAL, response["state"])
